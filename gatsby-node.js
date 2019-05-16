347a7a686a5432fe26d3c8817adf4c89042d0e90
Adam Wilson <adam@wlsn.org>
2019-05-16 11:06:12 -0400

    quick fix for deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,68 +1,11 @@
-const itemFilters = require('./public/js/armory.json');
-const uniqueItems = require('./public/js/uniqueItems.json');
-const setItems = require('./public/js/setItems.json');
-
-/* const getPokemonData = names =>
-  Promise.all(
-    names.map(async name => {
-      const { data: pokemon } = await get(`/pokemon/${name}`);
-      const abilities = await Promise.all(
-        pokemon.abilities.map(async ({ ability: { name: abilityName } }) => {
-          const { data: ability } = await get(`/ability/${abilityName}`);
-
-          return ability;
-        })
-      );n
-
-      return { ...pokemon, abilities };
-    })
-  );
- */
+const itemFilters = require('./data/armory.json');
+const uniqueItems = require('./data/uniqueItems.json');
+const setItems = require('./data/setItems.json');
 
 exports.createPages = async ({ actions: { createPage } }) => {
-  // `getPokemonData` is a function that fetches our data
-  // const allPokemon = await getPokemonData(["pikachu", "charizard", "squirtle"])
-
-  // Create a page that lists all Pokémon.
   createPage({
     path: '/armory/uniques',
-    component: require.resolve('./src/pages/uniques.js'),
+    component: await require.resolve('./src/templates/uniques.js'),
     context: { uniqueItems, itemFilters },
   });
-
-  // Create a page for each Pokémon.
-  /* allPokemon.forEach((pokemon) => {
-          createPage({
-            path: `/pokemon/${pokemon.name}/`,
-            component: require.resolve('./src/templates/pokemon.js'),
-            context: { pokemon },
-          });
-        }); */
 };
-
-/*
-exports.createPages = async ({ actions: { createPage }, graphql }) => {
-  const results = await graphql(`
-    {
-      allUniquesJson {
-        edges {
-          node {
-          }
-        }
-      }
-    }
-  `);
-
-  results.data.allProductsJson.edges.forEach((edge) => {
-    const product = edge.node;
-
-    createPage({
-      path: `/gql/${product.slug}/`,
-      component: require.resolve('./src/templates/uniques-graphql.js'),
-      context: {
-        node: product,
-      },
-    });
-  });
-};
- */
---
+++ src/templates/uniques.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import UniqueItem from '../components/UniqueItem';
+import UpgradeableUniqueItem from '../components/UpgradeableUniqueItem';
+import UniqueFilters from '../components/UniqueFilters';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+export default ({ pageContext }) => {
+  const findUniqueItemByIndex = (itemIndex) => {
+    const match = pageContext.uniqueItems.filter((item) => {
+      if (item.index === itemIndex) {
+        return true;
+      }
+      return false;
+    });
+    return match[0];
+  };
+
+  const itemList = () => {
+    if (pageContext.uniqueItems) {
+      pageContext.uniqueItems.map((item) => {
+        // upgradeable items
+        if (item.upgradeIndex !== '' && item.root) {
+          // build an array of items in the upgrade set by getting the upgradeIndexes until it is ""
+          const items = [];
+          let thisItem = item;
+
+          while (thisItem.upgradeIndex !== '') {
+            items.push(thisItem);
+            thisItem = findUniqueItemByIndex(thisItem.upgradeIndex);
+          }
+          items.push(thisItem);
+          return <UpgradeableUniqueItem key={item.index} items={items} />;
+        }
+        // non-upgradeable items
+        if (item.upgradeIndex === '' && item.root) {
+          return <UniqueItem key={item.index} item={item} />;
+        }
+      });
+    }
+  };
+
+
+  return (
+    <Layout>
+      <SEO title="Diablo 2 Resurgence mod - Item Armory" keywords={['Diablo II', 'Resurgence', 'Armory']} />
+      <UniqueFilters itemFilters={pageContext.itemFilters} />
+      <ul style={{ padding: 0 }}>
+        {itemList}
+      </ul>
+    </Layout>
+  );
+};
