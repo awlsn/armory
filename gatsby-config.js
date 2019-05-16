a40ba74b0c942a9250fe5037f3559d06ecb3f7f8
Adam Wilson <adam@wlsn.org>
2019-05-16 11:09:05 -0400

    quick fix for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/public/images`,
+        path: './public/images',
       },
     },
     {
